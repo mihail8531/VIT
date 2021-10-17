664e927a7f208754eb52e828d0bf1a66a3a0b7f6
mihail8531 <mihail8531@gmail.com>
2021-10-17 20:51:05 +0300

    Обновил утилиту и readme

--- bf2nums.py
+++ bf2nums.py
@@ -1,12 +1,32 @@
-d = {">": "1", "<": "2", "+": "3", "-": "4", ".": "5", ",": "6"}
-res = ""
-with open("input.txt", "r") as f:
-    for line in f:
-        line = line.strip()
-        for char in line:
-            res += d[char] + " "
+import argparse
 
-with open("output.txt", "w") as f:
-    f.write(res[:-1])
-print("Готово")
+d = {">": "0", "<": "1", "+": "2", "-": "3", ".": "4", ",": "5", "[": "6", "]": "7"}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Утилита для перевода Brainfuck программ в файл для ОЗУ logisim')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-f", "--file", default=None, type=str, help="Считать Brainfuck код из файла")
+    group.add_argument("-s", "--string", default=None, type=str, help="Считать Brainfuck код одной строкой")
+    parser.add_argument("-o", "--out", default=None, type=str, help="Записать программу в файл")
+    args = parser.parse_args()
+    inp = ""
+    if args.file is not None:
+        with open(args.file, "r") as f:
+            for line in f:
+                inp += line.strip()
+    else:
+        inp = args.string
+    res = inp
+    for key in d:
+        res = res.replace(key, d[key])
+    res = " ".join(res)
+    if args.out is not None:
+        res = "v2.0 raw\n" + res
+        with open(args.out, "w") as f:
+            f.write(res)
+        print(f"Записано в {args.out}")
+    else:
+        print(res)
+
+
 
